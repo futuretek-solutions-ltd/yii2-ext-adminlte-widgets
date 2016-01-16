f3b4f7ffd5ed9797636ea8fc76d9fc16ab5f472c
Lukáš Černý <lukas.cerny@futuretek.cz>
2016-01-16 18:45:10 +0100

    Initial FTS commit

--- ExtAdminlteAsset.php
+++ ExtAdminlteAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class ExtAdminlteAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/insolita/yii2-adminlte-widgets/js';
+    public $sourcePath = '@vendor/futuretek/ext-adminlte-widgets/js';
 
     public $js
         = [
